7874a45cdc24568539ffe825c38d83dbad42d280
AngelSushi <angelsushigame@gmail.com>
2022-04-26 12:02:46 +0200

    Add Cooldown Skill

--- Assets/Script/PlayerAttack.cs
+++ Assets/Script/PlayerAttack.cs
@@ -2,17 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerAttack : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
+public class PlayerAttack : MonoBehaviour {
+
+    [Header("Competence 2")]
+    public int cooldownTwo;
+    private float timerTwo;
+    [HideInInspector]
+    public bool isInCooldownTwo;
+
+
+    void Start() {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         
+        if(isInCooldownTwo) {
+            timerTwo += Time.deltaTime;
+
+            if(timerTwo >= cooldownTwo) {
+                isInCooldownTwo = false;
+                timerTwo = 0.0f;
+            }
+        }
     }
+
+    void Attack(int attackID) {
+
+    }
+
+   
 }
